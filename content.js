27eee9e8be9ca53c88eb30a27a8ba9f28591152f
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2024-11-15 17:25:40 -0500

    Adds first ride

---
+++ content.js
@@ -0,0 +1,3 @@
+const content = [
+	["11/16 returning to the peace pagoda", "Current route idea is around 33 miles, subject to change. Would be leaving in the afternoon. Points of interest: North Valley Road, Lake Wyola, Leverett Peace Pagoda. Might stop to look at the view from the top of East Plumtree Road.", "ridewithgps.com/routes/49063238"]
+];
