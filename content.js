b6409c7c77f62f49d013b0561d9fdb166493a17d
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2024-11-15 19:49:26 -0500

    11/16 ride updates

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 const content = [
-	["11/16 returning to the peace pagoda", "Current route idea is around 33 miles, subject to change. Would be leaving in the afternoon. Points of interest: North Valley Road, Lake Wyola, Leverett Peace Pagoda. Might stop to look at the view from the top of East Plumtree Road.", "ridewithgps.com/routes/49063238"]
+	["11/16 returning to the peace pagoda", "Current route idea is around 33 miles, subject to change. Leaving from the patio on the south side of the studio arts building, overlooking the intersection of N. Pleasant and Thatcher road. I'm planning to be there at noon, will update this page if that changes. Points of interest: North Valley Road, Lake Wyola, Leverett Peace Pagoda. Might stop to look at the view from the top of East Plumtree Road.", "ridewithgps.com/routes/49063238"]
 ];
