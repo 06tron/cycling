3fcf9f2887e519c66ae70f26dcd297859c33d497
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2024-11-17 17:12:22 -0500

    Slight restructuring of website

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-	let i = new URLSearchParams(window.location.search).get("i") ?? content.length - 1;
-	let selected = `<h2><span class="fl">${content[i][0]}</span></h2><p>${content[i][1]}`;
-	for (let j = 2; j < content[i].length; ++j) {
-		selected += `<br><a href="https://${content[i][j]}">${content[i][j]}</a>`;
+	const params = new URLSearchParams(window.location.search);
+	const count = params.get("c") ?? 2;
+	const start = params.get("i") ?? content.length - count;
+	let selected = "";
+	for (let i = start; i < start + count; ++i) {
+		selected += `<h2><span class="fl">${content[i][0]}</span></h2><p>${content[i][1]}`;
+		for (let j = 2; j < content[i].length; ++j) {
+			selected += `<br><a href="https://${content[i][j]}">${content[i][j]}</a>`;
+		}
+		selected += "</p>";
 	}
-	document.getElementById("selected").innerHTML = selected + "</p>";
+	document.getElementById("selected").innerHTML = selected;
 	let other = "";
-	for (let k = 0; k < content.length; ++k) {
-		if (k == i) continue;
-		other += `<br><a href="https://bike.6t.lt/?i=${k}">${content[k][0]}</a>`;
+	for (let k = content.length - 1; k >= 0; --k) {
+		if (k >= start && k < start + count) continue;
+		other += `<br><a href="https://bike.6t.lt/?c=1&i=${k}">${content[k][0]}</a>`;
 	}
 	document.getElementById("other").innerHTML = other.slice(4);
 });
@@ -17,7 +23,7 @@ const button = document.getElementById("copy");
 button.style.width = `${button.offsetWidth + 20}px`;
 button.addEventListener("mousedown", function () {
 	let all = `<h1><a href="https://bike.6t.lt">bike.6t.lt</a></h1><dl>`;
-	for (let i = 0; i < content.length; ++i) {
+	for (let i = content.length - 1; i >= 0; --i) {
 		all += `<dt>${content[i][0]}</dt><dd>${content[i][1]}`;
 		for (let j = 2; j < content[i].length; ++j) {
 			all += `<br><a href="https://${content[i][j]}">${content[i][j]}</a>`;
@@ -29,5 +35,5 @@ button.addEventListener("mousedown", function () {
 	button.innerText = "Copied!";
 	setTimeout(function () {
 		button.innerText = "Copy all content";
-	}, 2000);
+	}, 1200);
 });
