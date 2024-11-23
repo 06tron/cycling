501a7192ff3fa03f5708685a997b5bb1ba9e8682
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2024-11-23 13:50:48 -0500

    Adds resources section (highlighted links)

--- content.js
+++ content.js
@@ -2,5 +2,10 @@ const content = [
 	["04/27 morning ride to goodnough dam", "saw beavers, turkeys, deer, and left early so I could get back to umass for the bay state livestock classic", "www.strava.com/activities/11277961928", "ridewithgps.com/routes/46379386"],
 	["09/21 ride to the swift river valley historical society", "went to see Tom Ricardi’s birds of prey. The planned route has a couple extra hills in south wendell that I want to check out sometime", "www.strava.com/activities/12470640073", "ridewithgps.com/routes/48427204"],
 	["11/16 returning to the peace pagoda", "Current route idea is around 33 miles, subject to change. Leaving from the patio on the south side of the studio arts building, overlooking the intersection of N. Pleasant and Thatcher road. I'm planning to be there at noon, will update this page if that changes. Points of interest: North Valley Road, Lake Wyola, Leverett Peace Pagoda. Might stop to look at the view from the top of East Plumtree Road.", "ridewithgps.com/routes/49063238"],
-	["TBD mostly trail ride to rattlesnake gutter", "route planning in progress, would go through the Paul C. Jones working forest", "ridewithgps.com/routes/49080614", "massgis.maps.arcgis.com/apps/OnePane/basicviewer/index.html?appid=47689963e7bb4007961676ad9fc56ae9", "drive.google.com/file/d/1od3F265F1d3W2v2HcA5CU_ZK8WHZnsUc/view", "rattlesnakeguttertrust.org/files/Mosher-AreaTrails-Shelter-lidar.pdf", "rattlesnakeguttertrust.org/files/Rattlesnake-Gutter-AreaTrails-lidar.pdf"]
+	["TBD mostly trail ride to rattlesnake gutter", "route planning in progress, would go through the Paul C. Jones working forest", "ridewithgps.com/routes/49080614", "massgis.maps.arcgis.com/apps/OnePane/basicviewer/index.html?appid=47689963e7bb4007961676ad9fc56ae9", "wdcowls.com/forest-management/paul-c-jones-working-forest/", "rattlesnakeguttertrust.org/files/Mosher-AreaTrails-Shelter-lidar.pdf", "rattlesnakeguttertrust.org/files/Rattlesnake-Gutter-AreaTrails-lidar.pdf"]
+];
+
+const links = [
+	["Massachusetts Interactive Property Map", 3, 3],
+	["W.D. Cowls Timberland Information (links to public use policy)", 3, 4]
 ];
